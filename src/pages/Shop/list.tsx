87fe75057289ce021b09811a854450833ec57af8
Spicely <Spicely@outlook.com>
2020-12-08 23:03:25 +0800

    []

--- src/pages/Shop/list.tsx
+++ src/pages/Shop/list.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { message, Modal } from 'antd'
 import styled from 'styled-components'
 import { LayoutNavBar } from 'src/layouts/PageLayout'
-import { Button, Dialog, LabelHeader, Form, Table, Label, Tag } from 'components'
+import { Button, Dialog, LabelHeader, Form, Table, Label } from 'components'
 import http, { httpUtils, baseUrl } from '../../utils/axios'
 import { connect, DispatchProp } from 'react-redux'
 import { IFormItem, IFormFun } from 'src/components/lib/Form'
@@ -11,8 +11,9 @@ import { GlobalView } from 'src/utils/node'
 import { IInitState, MukaOS } from 'src/store/state'
 import { NavBarThemeData, Color, getUnit, DialogThemeData } from 'src/components/lib/utils'
 import { SET_GOODS_LIST_DATA, SET_ROUTERS_DATA, SET_SPINLOADING_DATA } from 'src/store/action'
-import { IRouter } from 'src/store/reducers/router'
+import * as XLSX from 'xlsx'
 import moment from 'moment'
+import LRangePicker from 'src/components/lib/RangePicker'
 
 declare const AMap: any
 
@@ -36,6 +37,8 @@ interface IState {
     classifyName: string
     lastIds: number[]
     parents: { label: number, value: any }[]
+    startTime: string
+    endTime: string
 }
 
 const FromLabel = styled.div`
@@ -58,7 +61,9 @@ class ShopList extends Component<IProps, IState> {
         classifyVisible: false,
         classifyName: '',
         lastIds: [],
-        parents: []
+        parents: [],
+        startTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+        endTime: moment().format('YYYY-MM-DD HH:mm:ss'),
     }
 
     private fn: IFormFun | null = null
@@ -134,13 +139,19 @@ class ShopList extends Component<IProps, IState> {
 
     public render(): JSX.Element {
         const { goodsList } = this.props
-        const { visible, visibleName } = this.state
+        const { visible, visibleName, startTime, endTime } = this.state
         return (
             <GlobalView>
                 <LayoutNavBar
                     left={null}
                     theme={navBarTheme}
                     title={<LabelHeader title="申请列表" line="vertical" />}
+                    right={
+                        <div className="flex">
+                            <LRangePicker style={{ height: '100%' }} value={[moment(startTime), moment(endTime)]} onChange={this.handleTime} />
+                            <Button mold="primary" onClick={this.handleXlsx}>导出数据</Button>
+                        </div>
+                    }
                 />
                 <Table
                     bordered
@@ -171,6 +182,13 @@ class ShopList extends Component<IProps, IState> {
                 </Dialog>
             </GlobalView>
         )
+    }
+
+    private handleTime = async (val: any) => {
+        this.setState({
+            startTime: moment(val[0]).format('YYYY-MM-DD HH:mm:ss'),
+            endTime: moment(val[1]).format('YYYY-MM-DD HH:mm:ss')
+        })
     }
 
     private handleDelete = async (id: any) => {
@@ -293,6 +311,46 @@ class ShopList extends Component<IProps, IState> {
                 })
             }, 10)
         })
+    }
+
+    private handleXlsx = async () => {
+        try {
+            const { startTime, endTime } = this.state
+            const data = await http('/admin/goods/findall', {
+                startTime,
+                end: endTime,
+            })
+            // 自定义下载的header，注意是数组中的数组哦
+            const Header = [['id', '姓名', '年龄', '性别', 'WhatsApp', '手机号', '自我介绍', '创建时间', 'IP']];
+
+            // 官方文档中的描述：converts an array of arrays of JS data to a worksheet.
+            const headerWs = XLSX.utils.aoa_to_sheet(Header);
+            const ws = XLSX.utils.sheet_add_json(headerWs, data.map((i: any) => {
+                return {
+                    "id": i.id,
+                    "name": i.name,
+                    "age": i.age,
+                    "sex": i.sex,
+                    "whatsApp": i.whatsApp,
+                    "mobile": i.mobile,
+                    "introduce": i.introduce,
+                    "created_at": i.created_at,
+                    "ip": i.ip,
+
+                }
+            }), { skipHeader: true, origin: "A2" });
+
+            /* 新建空workbook，然后加入worksheet */
+            const wb = XLSX.utils.book_new();
+
+            // 可以自定义下载之后的sheetname
+            XLSX.utils.book_append_sheet(wb, ws, "sheetName");
+
+            /* 生成xlsx文件 */
+            XLSX.writeFile(wb, "数据.xlsx")
+        } catch (msg) {
+            message.error(msg)
+        }
     }
 }
 
